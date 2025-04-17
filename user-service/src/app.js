c4e1dc9d58056432c00a99904bb99806d7b3f64e
IT21228612 <it21228612@my.sliit.lk>
2025-04-17 09:03:37 +0530

    update app.js of user service
    changed MONGO_URI to MONGO_URL

--- user-service/src/app.js
+++ user-service/src/app.js
@@ -34,7 +34,7 @@ const PORT = process.env.PORT || 5000;
 
 (async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
+    await mongoose.connect(process.env.MONGO_URL, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
