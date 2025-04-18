afd6753105e0eebda659d109a136034a48457d20
Dulhan Karunarathne <dhansaja2001@gmail.com>
2025-04-18 23:35:08 +0530

    Update userRoutes.js
    Add validate OTP and delete routes

--- user-service/src/Routes/userRoutes.js
+++ user-service/src/Routes/userRoutes.js
@@ -1,6 +1,3 @@
-// routes/authRoutes.js
-// Authentication routes for user registration, login, and password management
-
 const express = require('express');
 const router = express.Router();
 
@@ -9,13 +6,24 @@ const {
   registerUser,
   loginUser,
   forgotPassword,
+  verifyOTP, // Add verifyOTP
   resetPassword,
+  getUserProfile,
+  updateUserProfile,
+  deleteUserAccount,
 } = require('../Controllers/userController');
 
 // Auth routes
 router.post('/register', registerUser);          // Register new user
 router.post('/login', loginUser);                // Login existing user
 router.post('/forgot-password', forgotPassword); // Request password reset
+router.post('/verify-otp', verifyOTP);           // Verify OTP (New Route)
 router.post('/reset-password', resetPassword);   // Reset password with token
+router.get('/profile', getUserProfile);  // Get user profile route
+router.put('/profile', updateUserProfile);  // Update user profile route
+
+
+// Apply authentication middleware to the delete account route
+router.delete('/delete-account', deleteUserAccount);
 
 module.exports = router;
