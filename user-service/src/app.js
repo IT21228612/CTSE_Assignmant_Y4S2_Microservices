4c042861dffa2f438af1f86d9590a4af6f2c0b74
Dulhan Karunarathne <dhansaja2001@gmail.com>
2025-04-18 23:36:05 +0530

    Update app.js

--- user-service/src/app.js
+++ user-service/src/app.js
@@ -34,7 +34,7 @@ const PORT = process.env.PORT || 5000;
 
 (async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URL, {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
